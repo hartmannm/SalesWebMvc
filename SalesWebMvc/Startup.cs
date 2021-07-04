f6cce1e5a010d9c76b187bff27cf3981900edd25
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-04 15:57:21 -0300

    Implementado service para popular o banco de dados em perfil de desenvolvimento caso não existam dados

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 
@@ -42,14 +37,16 @@ namespace SalesWebMvc
                         builder => builder.MigrationsAssembly("SalesWebMvc")
                     )
             );
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
