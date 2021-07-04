2a076f338eab3443e9c3a142906d2edbd16efd32
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-04 17:22:03 -0300

    Implementada vinculação entre vendedor e departamento na inclusão - Alterado model de vendedor para receber o ID do departamento - Criada nova migration - Criado novo model para transferência de dados entre a view e o app - Criado service para manipulação de departamento

---
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> findAll()
+        {
+            return _context.Department
+                .OrderBy(department => department.Name)
+                .ToList();
+        }
+    }
+}
