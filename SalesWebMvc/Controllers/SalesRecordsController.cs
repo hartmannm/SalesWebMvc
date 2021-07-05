2903098d580c6c2378425b0caa0da26738ff5567
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-05 20:50:05 -0300

    Implementada busca de vendas - Criada página para busca de vendas por datas - Criada página para exibição de busca de vendas por datas simples - Criado controller para manipular vendas

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+
+        private SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            if (!minDate.HasValue)
+                minDate = new DateTime(DateTime.Now.Year, 01, 01);
+            if (!maxDate.HasValue)
+                maxDate = DateTime.Now;
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
