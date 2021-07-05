b4f175dbea00c0e23fd4e7da66c3e94787fc3f72
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-07-05 20:08:26 -0300

    Operações de persistência convertidas para métodos assíncronos

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,7 +1,9 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
@@ -14,11 +16,11 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department
+            return await _context.Department
                 .OrderBy(department => department.Name)
-                .ToList();
+                .ToListAsync();
         }
     }
 }
